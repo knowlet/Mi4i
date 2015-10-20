7033e0237d6fc4b3bcc86ffe9fa94a9e4e4fb5cd
knowlet <knowlet3389@gmail.com>
2015-10-20 19:44:14 +0800

    add countdown

--- Program.cs
+++ Program.cs
@@ -13,6 +13,21 @@ namespace Mi4i
             Bot bot = new Bot();
             bot.setUserData("f108854e@opayq.com", "xenl.48dfz");
             bot.login();
+            Console.WriteLine("使用倒數功能請輸入日期（如不需要請按Enter繼續）");
+            string t = Console.ReadLine();
+            if (!t.Equals(String.Empty))
+            {
+                DateTime dt = Convert.ToDateTime(t);
+                TimeSpan ts = dt.Subtract(DateTime.Now);
+                Console.WriteLine(ts.TotalSeconds);
+                while (ts.TotalSeconds > 0)
+                {
+                    Console.Write("\r");
+                    Console.Write("倒數：" + ts.TotalSeconds + "\t");
+                    ts = dt.Subtract(DateTime.Now);
+                }
+                Console.WriteLine("\r倒數完成\t\t");
+            }
             Console.WriteLine("請選擇購買方式：\n\t[1] 買一隻\n\t[2] 一直買");
             int choose = 0;
             Int32.TryParse(Console.ReadLine(), out choose);
